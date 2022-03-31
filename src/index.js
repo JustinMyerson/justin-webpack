83b86b20ed1f9ba936ef64651d93f4bac3f58fef
JustinMyerson <100346942+JustinMyerson@users.noreply.github.com>
2022-03-31 12:17:38 +0200

    Finish development tutorial

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import _ from 'lodash';
 import './style.css';
 import Icon from './icon.png'
+import printMe from './print';
 
 function component() {
   const element = document.createElement('div');
+  const btn = document.createElement('button');
 
   // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
+
+  btn.innerHTML = "Click meeeee"
+  btn.onclick = printMe
+
+  element.appendChild(btn)
 
   const myIcon = new Image();
   myIcon.src = Icon;
---
+++ src/print.js
@@ -0,0 +1,3 @@
+export default function printMe() {
+  console.log("Output to be printed")
+}
